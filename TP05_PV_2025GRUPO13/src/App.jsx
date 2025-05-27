a08a1275884a06c32fc2de35138d52f8a9002fd1
FranciscoDiaz-01 <d3st31@gmail.com>
2025-05-27 01:01:16 -0300

    Diaz Francisco, agrego routes.

--- TP05_PV_2025GRUPO13/src/App.jsx
+++ TP05_PV_2025GRUPO13/src/App.jsx
@@ -7,6 +7,27 @@ import EditarAlumno from "./EditarAlumno";
 import AcercaDe from "./AcercaDe";
 
 function App() {
-  
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Inicio />} />
+          <Route
+            path="alumnos"
+            element={<ListaAlumnos alumnos={alumnos} setAlumnos={setAlumnos} />}
+          />
+          <Route
+            path="alumnos/nuevo"
+            element={<NuevoAlumno setAlumnos={setAlumnos} />}
+          />
+          <Route
+            path="alumnos/:id/editar"
+            element={<EditarAlumno alumnos={alumnos} setAlumnos={setAlumnos} />}
+          />
+          <Route path="acerca" element={<AcercaDe />} />
+        </Route>
+      </Routes>
+    </Router>
+  );
 }
 export default App;
