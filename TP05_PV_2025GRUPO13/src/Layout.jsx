d81f5fe49581f73369fd9bb38933acfe7e66394f
Modena Stefano <modenastefano00@gmail.com>
2025-05-27 01:12:53 -0300

    NavBar  realizado por StefanoModena

---
+++ TP05_PV_2025GRUPO13/src/Layout.jsx
@@ -0,0 +1,14 @@
+import NavBar from "./NavBar";
+import { Outlet } from "react-router-dom";
+
+function Layout() {
+  return (
+    <div>
+      <NavBar />
+      <hr />
+      <Outlet />
+    </div>
+  );
+}
+
+export default Layout;
