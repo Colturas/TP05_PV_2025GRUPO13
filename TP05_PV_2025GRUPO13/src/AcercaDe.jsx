b649917accdf275424b9636024d83e5fec613e6d
Colturas <Pachekin8989@gmail.com>
2025-05-27 01:20:36 -0300

    Agrego el texto al arichivo AcercaDe- Fernandez Jorge

---
+++ TP05_PV_2025GRUPO13/src/AcercaDe.jsx
@@ -0,0 +1,10 @@
+function AcercaDe() {
+  return (
+    <div>
+      <h2>Acerca de esta aplicación</h2>
+      <p>Desarrollada por el equipo de alumnos para la gestión académica.</p>
+    </div>
+  );
+}
+
+export default AcercaDe;
