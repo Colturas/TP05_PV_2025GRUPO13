18de42111b839408f779a22411f33f86a4b4f116
Modena Stefano <modenastefano00@gmail.com>
2025-06-02 23:31:52 -0300

    Layout, NavBar realizado por stefano modena

--- TP05_PV_2025GRUPO13/src/NavBar.jsx
+++ TP05_PV_2025GRUPO13/src/NavBar.jsx
@@ -2,13 +2,11 @@ import { Link } from "react-router-dom";
 
 function NavBar() {
   return (
-    <nav>
-      <ul>
-        <li><Link to="/">Inicio</Link></li>
-        <li><Link to="/alumnos">Lista de Alumnos</Link></li>
-        <li><Link to="/alumnos/nuevo">Nuevo Alumno</Link></li>
-        <li><Link to="/acerca">Acerca de</Link></li>
-      </ul>
+    <nav style={{ marginBottom: "1rem" }}>
+      <Link to="/">Inicio</Link> |{" "}
+      <Link to="/alumnos">Lista de Alumnos</Link> |{" "}
+      <Link to="/alumnos/nuevo">Nuevo Alumno</Link> |{" "}
+      <Link to="/acerca">Acerca de</Link>
     </nav>
   );
 }
